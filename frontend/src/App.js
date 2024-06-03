3ef61544af6ef8ab938bc488754fd39aaf3a0310
Pasindusheshan127 <pasinduhettiarachchi201@gmail.com>
2024-06-03 10:45:13 +0530

    crud-app

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import "./style.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Books from "./pages/Books";
+import AddBook from "./pages/AddBook";
+import UpdateBook from "./pages/UpdateBook";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/add" element={<AddBook />} />
+          <Route path="/update/:id" element={<UpdateBook />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
